c4c50ad4f3e25e6bf6beccadcb1cf25ec7fff4f1
DorianFenard <144025115+DorianFenard@users.noreply.github.com>
2024-05-27 16:06:13 +0200

    Add files via upload

--- zeldiablo/src/gameLaby/laby/MainLaby.java
+++ zeldiablo/src/gameLaby/laby/MainLaby.java
@@ -6,12 +6,12 @@ public class MainLaby {
 
     public static void main(String[] args) {
 
-        MoteurJeu.setTaille(600, 400);
         MoteurJeu.setFPS(60);
 
         try {
 
             LabyJeu jeu = new LabyJeu("labySimple/laby2.txt");
+            MoteurJeu.setTaille( jeu.getLabyrinthe().getLength()*LabyDessin.TAILLE,  jeu.getLabyrinthe().getLengthY()*LabyDessin.TAILLE);
             LabyDessin dessin = new LabyDessin();
             MoteurJeu.launch(jeu, dessin);
 
